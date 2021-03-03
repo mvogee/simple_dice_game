e8b31a9461f817e2bd36f6ae5462942460cc07a9
Matthew Vogee <matthewvogee@Matthews-MacBook-Pro.local>
2021-03-02 20:28:28 -0800

    simple javascript for dice rolls and to change images and text on button click

---
+++ js/dice.js
@@ -0,0 +1,42 @@
+
+
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function putDiceImg(diceImg, rollNum) {
+    diceImg.src = "./images/dice" + rollNum + ".png";
+}
+
+function updateWinner(p1, p2) {
+    var header = document.querySelector('h1');
+    if (p1 > p2) {
+        header.innerHTML = "Player 1 Wins!";
+    }
+    else if (p2 > p1) {
+        header.innerHTML = "Player 2 Wins!";
+    }
+    else {
+        header.innerHTML = "Its a Tie!";
+    }
+}
+
+function main() {
+    //get dice rolls
+    var dice1 = document.querySelector('.first-dice img');
+    var dice2 = document.querySelector('.second-dice img');
+    var playerOneRoll = rollDice();
+    var playerTwoRoll = rollDice();
+
+    console.log(playerOneRoll);
+    console.log(playerTwoRoll);
+    //change images to roll
+    putDiceImg(dice1, playerOneRoll);
+    putDiceImg(dice2, playerTwoRoll);
+    //change text to which player won
+    updateWinner(playerOneRoll, playerTwoRoll);
+}
+
+var rollbtn = document.querySelector('.btn');
+rollbtn.onclick = function() {main();};
